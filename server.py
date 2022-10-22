f8b7abc8dfc0a318db1e2c6c02327062af7d10ca
Jake O'Donnell <10jakeo@gmail.com>
2022-10-22 12:34:40 -0400

    Added results page

--- server.py
+++ server.py
@@ -1,10 +1,22 @@
 from flask import Flask, render_template, redirect, session, request
 
 app = Flask(__name__)
+app.secret_key = 'akfnbj4fnsdv;lkndfv'
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/submit', methods = ['POST'])
+def p_submit():
+    session['f_name'] = request.form['f_name']
+    session['l_name'] = request.form['l_name']
+    session['current_stack'] = request.form['current_stack']
+    return redirect('/result')
+
+@app.route('/result')
+def r_result():
+    return render_template('result.html', f_name = session['f_name'], l_name = session['l_name'], stack = session['current_stack'])
 
 if __name__ == '__main__':
     app.run(debug = True)
