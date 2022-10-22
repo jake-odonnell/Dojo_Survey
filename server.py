bb9dd564183f2a84b90d36310d131b0692f6dbba
Jake O'Donnell <10jakeo@gmail.com>
2022-10-22 13:23:53 -0400

    Final Project

--- server.py
+++ server.py
@@ -7,16 +7,19 @@ app.secret_key = 'akfnbj4fnsdv;lkndfv'
 def index():
     return render_template('index.html')
 
-@app.route('/submit', methods = ['POST'])
+@app.route('/process', methods = ['POST'])
 def p_submit():
     session['f_name'] = request.form['f_name']
     session['l_name'] = request.form['l_name']
     session['current_stack'] = request.form['current_stack']
+    session['know'] = request.form.getlist('stack')
+    print(request.form['stack'])
     return redirect('/result')
 
 @app.route('/result')
 def r_result():
-    return render_template('result.html', f_name = session['f_name'], l_name = session['l_name'], stack = session['current_stack'])
+    print(session['know'])
+    return render_template('result.html', f_name = session['f_name'], l_name = session['l_name'], stack = session['current_stack'], stacks = session['know'])
 
 if __name__ == '__main__':
     app.run(debug = True)
